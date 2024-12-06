09178e4f8a3dc9a224c10b083e0a700891935e46
James O'Claire <jamesoclaire@gmail.com>
2024-12-06 16:34:54 +0800

    Add compressed data files

--- helper_scripts/make_tsv.py
+++ helper_scripts/make_tsv.py
@@ -1,14 +1,11 @@
-import pandas as pd
 from dbcon.queries import query_store_apps_companies, query_store_apps, query_live_store_apps
 
 from config import get_logger
 
-import os
-
 logger = get_logger(__name__)
 
 TABLES_DICT = {
-    # "store_apps_companies": query_store_apps_companies,
+    # "store_apps_companies": query_store_apps_companies, still too big
     "store_apps": query_store_apps,
     "live_store_apps": query_live_store_apps,
 }
@@ -17,10 +14,11 @@ def make_tsv(file_name: str):
     logger.info(f"{file_name} start")
     df = TABLES_DICT[file_name]()
     logger.info(f"{file_name} write")
-    df.to_csv(file_name + '.tsv.xz', sep="\t", index=False, compression="xz")
+    df.to_csv('data/' + file_name + '.tsv.xz', sep="\t", index=False, compression="xz")
     logger.info(f"{file_name} done")
 
 if __name__ == "__main__":
+    logger.info("start")
     for file_name in TABLES_DICT.keys():
         make_tsv(file_name)
 
